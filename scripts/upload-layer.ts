96eae2300e05cd3fae17dba69ad9279562b56ca9
Jay <jason.martin@clearmatics.com>
2024-10-11 15:00:38 +0100

    update upload process

--- scripts/upload-layer.ts
+++ scripts/upload-layer.ts
@@ -1,33 +1,19 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import dotenv from "dotenv";
-import { buildPartnersJson, generatePartnerList, getPartnerDirectories, getPartnerObject } from "./generate-partners";
-import { validatePartnerInfo } from "./validate-partners";
-import { partnerFileName } from "./constants";
-const fs = require("fs");
-dotenv.config();
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import dotenv from 'dotenv'
+import { buildPartnersJson, getPartnerDirectories } from './generate-partners'
+import { validatePartnerInfo } from './validate-partners'
+dotenv.config()
 
-const REGION = process.env.REGION ?? "";
-const BUCKET_NAME = process.env.BUCKET_NAME ?? "";
-const OBJECT_KEY = process.env.OBJECT_KEY ?? "";
-const ACCOUNT_ID = process.env.ACCOUNT_ID ?? "";
+const REGION = process.env.REGION ?? ''
+const BUCKET_NAME = process.env.BUCKET_NAME ?? ''
+const OBJECT_KEY = process.env.OBJECT_KEY ?? ''
+const ACCOUNT_ID = process.env.ACCOUNT_ID ?? ''
 
-const validationCheck = async (partnerDirectories: string[]) => { 
-    const promise = new Promise((resolve, reject) => {
-    //validate
-    partnerDirectories.forEach((partnerPath) => {
-        validatePartnerInfo(partnerPath).then((errorMessages) => {
-            if (errorMessages.length > 0) {
-                console.log(errorMessages.join(',\n').trim());
-                reject(new Error('Error with partner info'));
-            }
-            resolve(true);
-        }).catch((error) => {
-            reject(error)
-        }
-        );
-    });
-    });
-    return promise;
+const validationCheck = async (partnerDirectories: string[]) => {
+    const results = await Promise.all(
+        partnerDirectories.map(validatePartnerInfo),
+    )
+    return results
 }
 
 const run = async () => {
@@ -35,30 +21,29 @@ const run = async () => {
         region: REGION,
         endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
         credentials: {
-            accessKeyId: process.env.ACCESS_KEY_ID ?? "",
-            secretAccessKey: process.env.SECRET_ACCESS_KEY ?? "",
+            accessKeyId: process.env.ACCESS_KEY_ID ?? '',
+            secretAccessKey: process.env.SECRET_ACCESS_KEY ?? '',
         },
-    });
+    })
 
     try {
-        const partnerDirectories = getPartnerDirectories();
-        const validationResult = await validationCheck(partnerDirectories);
-        if (!validationResult) {
-            process.exit(1);
-        }
-        const partners = buildPartnersJson(partnerDirectories);                
+        console.log('getting directories...')
+        const partnerDirectories = getPartnerDirectories()
+        console.log('Validating partner info...')
+        await validationCheck(partnerDirectories)
+        console.log('Proceeding with upload...')
+        const partners = buildPartnersJson(partnerDirectories)
         const uploadParams = {
             Bucket: BUCKET_NAME,
             Key: OBJECT_KEY,
             Body: JSON.stringify(partners),
-            ContentType: "application/json", 
-        };
-         const data = await s3Client.send(new PutObjectCommand(uploadParams));
-         console.log(`Partners updated. ETag: ${data.ETag}`);
+            ContentType: 'application/json',
+        }
+        const data = await s3Client.send(new PutObjectCommand(uploadParams))
+        console.log(`Partners uploaded. ETag: ${data.ETag}`)
     } catch (err) {
-        process.stderr.write(`Error uploading object: ${err}\n`);
-        process.exit(1);
+        process.stderr.write(`Error uploading object: ${err}\n`)
+        process.exit(1)
     }
-};
-
-run();
+}
+run()
